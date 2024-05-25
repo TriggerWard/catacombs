7fd52769c5ffae81b98db39e3d735ce6f66aa5b2
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-05-25 19:51:21 +0200

    fix: create

--- packages/nextjs/app/create-crypt/components/create-crypt-form.tsx
+++ packages/nextjs/app/create-crypt/components/create-crypt-form.tsx
@@ -48,135 +48,138 @@ export function CreateCryptForm({ nillion, nillionClient }: { nillion: any; nill
 
   const handleCreateCrypt = async () => {
     try {
-      if (file && triggerText && cryptManagerData) {
-        setSealCryptStatus("pending");
-        const fileAsArrayBuffer = await fileToArrayBuffer(file);
-
-        // Generate key
-        const { iv, key, exportedKey, base64ExportedKey } = await generateKey();
-        console.log("Generated key", { iv, key, exportedKey, base64ExportedKey });
-        setSealCryptProgress(10);
-
-        // Encrypt file
-        const { base64Encrypted, encrypted } = await encrypt(key, iv, fileAsArrayBuffer);
-        console.log("Encrypted file", { base64Encrypted, encrypted });
-        setSealCryptProgress(40);
-
-        // Store encrypted file on IPFS
-        const cryptFileName = "crypt-file_" + Date.now();
-        const pinnedFileIpfs = await pinFileToIPFS(encrypted, cryptFileName);
-        console.log("Pinned encrypted file: ", pinnedFileIpfs.IpfsHash);
-        setSealCryptProgress(50);
-
-        // Store metadata on IPFS
-        const pinnedMetadataIpfs = await pinCryptMetadataToIPFS(
-          {
-            fileIpfsCid: pinnedFileIpfs.IpfsHash,
-            fileType: file.type,
-            triggerText,
-            iv,
-          },
-          cryptFileName + "_metadata.json",
-        );
-        console.log("Pinned metadata file:", pinnedMetadataIpfs.IpfsHash);
-        setSealCryptProgress(60);
-
-        // Store secret key on Nillion
-        const nillionSecretName = `crypt_key_${pinnedMetadataIpfs.IpfsHash}`;
-        const nillionStoreId = await storeSecretsBlob(nillion, nillionClient, [
-          {
-            name: nillionSecretName,
-            value: base64ExportedKey,
-          },
-        ]);
-        console.log("Stored secret key on Nillion", { nillionStoreId, nillionSecretName });
-        setSealCryptProgress(80);
-
-        // Call CryptManager.sol
-        const { request } = await prepareWriteContract({
-          address: cryptManagerData.address,
-          abi: [
-            {
-              inputs: [
-                { internalType: "string", name: "ipfsDataHash", type: "string" },
-                { internalType: "bytes", name: "decryptTrigger", type: "bytes" },
-                { internalType: "string", name: "nillionCrypt", type: "string" },
-                { internalType: "address", name: "warden", type: "address" },
-                { internalType: "address", name: "decryptCallback", type: "address" },
-              ],
-              name: "createCrypt",
-              outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-              stateMutability: "nonpayable",
-              type: "function",
-            },
-            ,
-          ] as const,
-          functionName: "createCrypt",
-          args: [
-            pinnedMetadataIpfs.IpfsHash,
-            toHex(triggerText),
-            nillionStoreId,
-            "0x5d996408dbbBB9Fdbb15F7BA45A01e25f86E3131", // Default
-            zeroAddress,
-          ],
-        });
-        const { hash } = await writeContract(request);
-        setSealCryptProgress(89);
-        console.log("Transaction hash", hash);
-        const receipt = await waitForTransaction({ hash, confirmations: 1 });
-        console.log("Transaction receipt", receipt);
-        setSealCryptProgress(100);
-
-        const [encodedEventTopic] = encodeEventTopics({
-          eventName: "CryptCreated",
-          abi: [
-            {
-              anonymous: false,
-              inputs: [
-                { indexed: true, internalType: "uint256", name: "cryptId", type: "uint256" },
-                { indexed: false, internalType: "string", name: "ipfsDataHash", type: "string" },
-                { indexed: false, internalType: "string", name: "nillionCrypt", type: "string" },
-                { indexed: false, internalType: "address", name: "decryptCallback", type: "address" },
-                { indexed: true, internalType: "address", name: "warden", type: "address" },
-                { indexed: true, internalType: "address", name: "owner", type: "address" },
-              ],
-              name: "CryptCreated",
-              type: "event",
-            },
-          ],
-        });
-        const cryptCreatedLog = receipt.logs.find(log => log.topics[0] === encodedEventTopic);
-        const cryptId = hexToBigInt(cryptCreatedLog?.topics[1] || "0x0");
-        console.log("Crypt created", cryptId);
-
-        //// TESTS
-
-        // test retrieve secret key from Nillion
-        const base64RetrievedKey = await retrieveSecretBlob(nillionClient, nillionStoreId, nillionSecretName);
-        const importedKey = await importKey(base64RetrievedKey);
-        console.log("Retrieved key from Nillion", { base64RetrievedKey, importedKey });
-
-        // test fetch metadata
-        const metadata = await (await fetchFromIPFS(pinnedMetadataIpfs.IpfsHash)).json();
-        console.log("Fetched crypt metadata:", metadata);
-        const fetchedEncryptedFile = await fetchFromIPFS(metadata.fileIpfsCid);
-        const fetchedEncryptedFileBuffer = await fetchedEncryptedFile.arrayBuffer();
-
-        // test decrypt file
-        const { decrypted, utf8Decrypted } = await decrypt(
-          importedKey,
-          Buffer.from(metadata.iv, "base64"),
-          fetchedEncryptedFileBuffer,
-        );
-        console.log("Decrypted file", {
-          decrypted,
-          utf8Decrypted,
-        });
-
-        setSealCryptStatus("success");
-      } else {
-        setSealCryptProgress(0);
+      if (!file || !triggerText) {
+        throw new Error("File or trigger text required");
       }
+      if (!cryptManagerData) {
+        throw new Error("CryptManager not found");
+      }
+
+      setSealCryptStatus("pending");
+      const fileAsArrayBuffer = await fileToArrayBuffer(file);
+
+      // Generate key
+      const { iv, key, exportedKey, base64ExportedKey } = await generateKey();
+      console.log("Generated key", { iv, key, exportedKey, base64ExportedKey });
+      setSealCryptProgress(10);
+
+      // Encrypt file
+      const { base64Encrypted, encrypted } = await encrypt(key, iv, fileAsArrayBuffer);
+      console.log("Encrypted file", { base64Encrypted, encrypted });
+      setSealCryptProgress(40);
+
+      // Store encrypted file on IPFS
+      const cryptFileName = "crypt-file_" + Date.now();
+      const pinnedFileIpfs = await pinFileToIPFS(encrypted, cryptFileName);
+      console.log("Pinned encrypted file: ", pinnedFileIpfs.IpfsHash);
+      setSealCryptProgress(50);
+
+      // Store metadata on IPFS
+      const pinnedMetadataIpfs = await pinCryptMetadataToIPFS(
+        {
+          fileIpfsCid: pinnedFileIpfs.IpfsHash,
+          fileType: file.type,
+          triggerText,
+          iv,
+        },
+        cryptFileName + "_metadata.json",
+      );
+      console.log("Pinned metadata file:", pinnedMetadataIpfs.IpfsHash);
+      setSealCryptProgress(60);
+
+      // Store secret key on Nillion
+      const nillionSecretName = `crypt_key_${pinnedMetadataIpfs.IpfsHash}`;
+      const nillionStoreId = await storeSecretsBlob(nillion, nillionClient, [
+        {
+          name: nillionSecretName,
+          value: base64ExportedKey,
+        },
+      ]);
+      console.log("Stored secret key on Nillion", { nillionStoreId, nillionSecretName });
+      setSealCryptProgress(80);
+
+      // Call CryptManager.sol
+      const { request } = await prepareWriteContract({
+        address: cryptManagerData.address,
+        abi: [
+          {
+            inputs: [
+              { internalType: "string", name: "ipfsDataHash", type: "string" },
+              { internalType: "bytes", name: "decryptTrigger", type: "bytes" },
+              { internalType: "string", name: "nillionCrypt", type: "string" },
+              { internalType: "address", name: "warden", type: "address" },
+              { internalType: "address", name: "decryptCallback", type: "address" },
+            ],
+            name: "createCrypt",
+            outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          ,
+        ] as const,
+        functionName: "createCrypt",
+        args: [
+          pinnedMetadataIpfs.IpfsHash,
+          toHex(triggerText),
+          nillionStoreId,
+          "0x5d996408dbbBB9Fdbb15F7BA45A01e25f86E3131", // Default
+          zeroAddress,
+        ],
+      });
+      const { hash } = await writeContract(request);
+      setSealCryptProgress(89);
+      console.log("Transaction hash", hash);
+      const receipt = await waitForTransaction({ hash, confirmations: 1 });
+      console.log("Transaction receipt", receipt);
+      setSealCryptProgress(100);
+
+      const [encodedEventTopic] = encodeEventTopics({
+        eventName: "CryptCreated",
+        abi: [
+          {
+            anonymous: false,
+            inputs: [
+              { indexed: true, internalType: "uint256", name: "cryptId", type: "uint256" },
+              { indexed: false, internalType: "string", name: "ipfsDataHash", type: "string" },
+              { indexed: false, internalType: "string", name: "nillionCrypt", type: "string" },
+              { indexed: false, internalType: "address", name: "decryptCallback", type: "address" },
+              { indexed: true, internalType: "address", name: "warden", type: "address" },
+              { indexed: true, internalType: "address", name: "owner", type: "address" },
+            ],
+            name: "CryptCreated",
+            type: "event",
+          },
+        ],
+      });
+      const cryptCreatedLog = receipt.logs.find(log => log.topics[0] === encodedEventTopic);
+      const cryptId = hexToBigInt(cryptCreatedLog?.topics[1] || "0x0");
+      console.log("Crypt created", cryptId);
+
+      //// TESTS
+
+      // test retrieve secret key from Nillion
+      const base64RetrievedKey = await retrieveSecretBlob(nillionClient, nillionStoreId, nillionSecretName);
+      const importedKey = await importKey(base64RetrievedKey);
+      console.log("Retrieved key from Nillion", { base64RetrievedKey, importedKey });
+
+      // test fetch metadata
+      const metadata = await (await fetchFromIPFS(pinnedMetadataIpfs.IpfsHash)).json();
+      console.log("Fetched crypt metadata:", metadata);
+      const fetchedEncryptedFile = await fetchFromIPFS(metadata.fileIpfsCid);
+      const fetchedEncryptedFileBuffer = await fetchedEncryptedFile.arrayBuffer();
+
+      // test decrypt file
+      const { decrypted, utf8Decrypted } = await decrypt(
+        importedKey,
+        Buffer.from(metadata.iv, "base64"),
+        fetchedEncryptedFileBuffer,
+      );
+      console.log("Decrypted file", {
+        decrypted,
+        utf8Decrypted,
+      });
+
+      setSealCryptStatus("success");
     } catch (e) {
       console.error(e);
       setSealCryptProgress(0);
